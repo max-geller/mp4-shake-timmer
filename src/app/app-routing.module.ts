8e90b3ebd5144c5a8bb3295d34b9cd8c7cbba724
Max Geller <mxgeller@gmail.com>
2021-11-19 16:38:05 -0700

    adding footer and navbar components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ImportComponent } from './pages/import/import.component';
+import { TrimComponent } from './pages/trim/trim.component';
 
-const routes: Routes = [{ path: 'modules', loadChildren: () => import('./modules/modules.module').then(m => m.ModulesModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'import',
+    component: ImportComponent
+  },
+  {
+    path: 'trim',
+    component: TrimComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
